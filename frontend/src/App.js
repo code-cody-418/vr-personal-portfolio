90f64fa996073240bbc90dc1cd05b4e415dad528
Brody-code-designer <80767371+Brody-code-designer@users.noreply.github.com>
2021-08-16 12:46:12 -0600

    set up react app

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import {BrowserRouter} from "react-router-dom";
+import {Route, Switch} from 'react-router'
+import {Home} from "./ui/Home";
+
+function App() {
+    return (
+        <>
+            <BrowserRouter>
+                <Switch>
+                  <Route exact='/' component={Home} />
+                </Switch>
+            </BrowserRouter>
+        </>
+    );
+}
+
+export default App;
