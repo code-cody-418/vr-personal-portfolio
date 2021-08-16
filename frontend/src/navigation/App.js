7c5e8861a4c39042fb10bf4eff552db9815f7e24
Brody-code-designer <80767371+Brody-code-designer@users.noreply.github.com>
2021-08-16 13:36:07 -0600

    added react routing and other libraries

--- frontend/src/navigation/App.js
+++ frontend/src/navigation/App.js
@@ -1,6 +1,6 @@
 import {BrowserRouter} from "react-router-dom";
 import {Route, Switch} from 'react-router'
-import {Home} from "./ui/Home";
+import {Home} from "../ui/Home";
 
 function App() {
     return (
