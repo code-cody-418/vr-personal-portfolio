a4bd3248e7b75f29bff18a901b58be95e68952a4
Brody-code-designer <80767371+Brody-code-designer@users.noreply.github.com>
2022-01-04 18:50:59 -0700

    added commented out velocity code

--- frontend/src/ui/pages/three-d-room/components/Player.js
+++ frontend/src/ui/pages/three-d-room/components/Player.js
@@ -3,7 +3,7 @@ import {useFrame, useThree} from "@react-three/fiber";
 import {useKeyboardControls} from "../hooks/useKeyboardControls";
 import {FPVControls} from "./FPVControls";
 import {Vector3} from "three";
-import {useSphere} from "@react-three/cannon";
+import {useBox, useSphere} from "@react-three/cannon";
 import Kakashi from "../models/Kakashi";
 
 const SPEED = 0.1
@@ -23,30 +23,51 @@ export const Player = (props) => {
 
     const ref = useRef()
 
+    //this turns the walking animation to true or false
     const [walking, setWalking] = useState(false)
 
-    useEffect( () => {
+    useEffect(() => {
 
-        if(moveForward === true || moveBackward === true ||moveLeft === true ||moveRight === true) {
+        if (moveForward === true || moveBackward === true || moveLeft === true || moveRight === true) {
             setWalking(true)
-            console.log("walking", walking)
         } else {
             setWalking(false)
-            console.log("stop walking", walking)
         }
     }, [moveForward, moveBackward, moveLeft, moveRight])
 
+    //working using velocity
+    // const [ref, api] = useBox(() => ({
+    //     mass: 1,
+    //     type: 'Dynamic',
+    //     position: [0, 0, 0],
+    //     // rotation: [Math.PI / 2, 0, Math.PI],
+    //     // ...props,
+    // }));
 
+    //working using velocity
+    //this subscribes to the changing value from the api. It then sets these values in an object for position
+    // const pos = useRef({x: 0, y: 1, z: 0})
+    // useEffect(() => api.position.subscribe(v => {
+    //     pos.current.x = v[0]
+    //     pos.current.y = v[1]
+    //     pos.current.z = v[2]
+    // }), [])
+
+// console.log("api", api.position.subscribe(v => console.log("v",v)))
 
     useFrame(() => {
 
+        // //working using velocity. Gets the camera position to follow the position of the ref
+        // camera.position.copy(pos.current);
+        // camera.position.z = pos.current.z + 3
+        // camera.position.y = pos.current.y + 1
 
 
-        camera.position.copy(ref.current.position)
-        camera.position.z = (ref.current.position.z + 3)
-        camera.position.y = ref.current.position.y + 1
+        // working using position
+                camera.position.copy(ref.current.position)
+                camera.position.z = (ref.current.position.z + 3)
+                camera.position.y = ref.current.position.y + 1
 
-        // camera.lookAt(ref.current.position.x, ref.current.position.y,  ref.current.position.z)
 
         const direction = new Vector3()
 
@@ -68,12 +89,14 @@ export const Player = (props) => {
             .multiplyScalar(SPEED)
             .applyEuler(camera.rotation)
 
+        //working using position
         ref.current.position.x = (ref.current.position.x + direction.x)
         ref.current.position.z = (ref.current.position.z + direction.z)
 
+        //working using velocity
         // api.velocity.set(direction.x, 0, direction.z)
+        // console.log("ref rotation", ref)
     })
-
 
 
     return (
@@ -85,10 +108,10 @@ export const Player = (props) => {
             </group>
 
 
-                {/*<mesh ref={ref} position={[0, 2, 0]}>*/}
-                {/*    <sphereGeometry />*/}
-                {/*    <meshStandardMaterial color={"#f30707"}/>*/}
-                {/*</mesh>*/}
+            {/*<mesh ref={ref} position={[0, 2, 0]}>*/}
+            {/*    <sphereGeometry/>*/}
+            {/*    <meshStandardMaterial color={"#f30707"}/>*/}
+            {/*</mesh>*/}
 
         </>
     )
